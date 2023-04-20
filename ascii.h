aa52c18ad762657793066e72bdb5e330e9712c21
BORISWKT <125748731+BORISWKT@users.noreply.github.com>
2023-04-20 17:50:10 +0800

    Add files via upload

---
+++ ascii.h
@@ -0,0 +1,14 @@
+//
+// Created by MartinYu on 18/4/2023.
+//
+
+#ifndef COURSE_PROJECT_ASCII_H
+#define COURSE_PROJECT_ASCII_H
+
+#endif //COURSE_PROJECT_ASCII_H
+
+void set3_ascii();
+void skill1_ascii();
+void skill2_ascii();
+void skill3_ascii();
+void skill4_ascii();
