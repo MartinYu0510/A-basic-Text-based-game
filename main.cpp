4d044f2201f5e0feeaabbcbc0882d7ec5a3b1e6e
MartinYu0510 <124866191+MartinYu0510@users.noreply.github.com>
2023-04-25 02:25:17 +0800

    Adding "TryAgain" feature

--- main.cpp
+++ main.cpp
@@ -1,32 +1,62 @@
 #include <iostream>
 #include <string>
-#include <chrono>
-#include <thread>
+#include <algorithm>
 #include "start_menu.h"
 #include "game_content.h"
 using namespace std;
-using namespace ::this_thread;
 
-void maingame(char mode){
+bool maingame(char mode){
+    bool player_loss;
     switch(mode){
         case '1':
-            new_game();
+            player_loss=new_game();
             break;
         case '2':
-            load_game();
+            player_loss=load_game();
             break;
         case '3':
+            cout << "This game is made by Martin Yu, Boris Wong and IDonnol." << '\n' << "This game is not funny because we aim to get good grade only." << '\n' << "If you want good gaming experience, I highly suggest you to play Elden Ring!!" << endl;
+            player_loss=NULL;
+            break;
+        case '4':
             cout << "Hope to seee you again !";
             exit(0);
     }
+    return player_loss;
 }
 
-
 int main() {
-    char mode;      //rmb to implement a function for returning to the main meun
-    mode=menu();
-    maingame(mode);
-    //ending()
+    char mode;
+    bool player_loss;
+    string option;
+    while (true) {
+        mode = menu();
+        player_loss = maingame(mode);
+        if (player_loss != NULL) {
+            ending(!player_loss);
+            while (true) {
+                if (player_loss) {
+                    cout << "Do you want to try again ?(Yes/No): ";
+                    cin >> option;
+                } else {
+                    cout << "Do you want to start a new game ?(Yes/No): ";
+                    cin >> option;
+                }
+                transform(option.begin(), option.end(), option.begin(), ::tolower);
+                if (option != "yes" and option != "no") {       //Checking invalid input
+                    cout << "Invalid input! Please input Yes or No !! ";
+                } else {
+                    break;
+                }
+            }
+            if (option == "yes") {
+                continue;
+            } else {
+                cout << "See you nex time !" << endl;
+                break;
+            }
+        }
+    }
     return 0;
 }
 
