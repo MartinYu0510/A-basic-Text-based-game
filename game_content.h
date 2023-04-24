68575d245922a1e3b92eaedae79697a63ff0e1e7
MartinYu0510 <124866191+MartinYu0510@users.noreply.github.com>
2023-04-25 02:16:42 +0800

    modify function type and bug fixing

--- game_content.h
+++ game_content.h
@@ -1,17 +1,13 @@
-//
-// Created by MartinYu on 4/4/2023.
-//
-
 #ifndef COURSE_PROJECT_MAIN_GAME_H
 #define COURSE_PROJECT_MAIN_GAME_H
 
 #endif //COURSE_PROJECT_MAIN_GAME_H
 struct Role;
-void set1(Role *player_addr, int &set, bool player_loss);
-void set2(Role *player_addr, int &set, bool player_loss);
-void set3(Role *player_addr, int &set, bool player_loss);
-void set4(Role *player_addr, int &set, bool player_loss);
-void skill_choice(Role *player_addr, int &set);
-void new_game();
-void load_game();
+void set1(Role *player_addr, int &set, bool &player_loss);
+void set2(Role *player_addr, int &set, bool &player_loss);
+void set3(Role *player_addr, int &set, bool &player_loss);
+void set4(Role *player_addr, int &set, bool &player_loss);
+void skill_choice(Role *player_addr, int &set, bool &player_loss);
+bool new_game();
+bool load_game();
 
